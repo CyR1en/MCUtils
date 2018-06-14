a3c93d69e5cac4b1c2a5f1f37c74e85125c9e5c0
cyr1en <cyrien3519@gmail.com>
2018-06-14 11:55:49 -0600

    1.4

--- src/main/java/com/cyr1en/mcutils/utils/reflection/annotation/Initialize.java
+++ src/main/java/com/cyr1en/mcutils/utils/reflection/annotation/Initialize.java
@@ -1,13 +1,9 @@
 package com.cyr1en.mcutils.utils.reflection.annotation;
 
-import xdean.deannotation.checker.CheckMethod;
-import xdean.deannotation.checker.CheckType;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-@CheckMethod(argCount = 0, returnType = @CheckType(void.class))
 public @interface Initialize {
     int priority();
 }
