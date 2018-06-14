a4034af4b57da95d48d8523b27dc6ad21ee28165
cyr1en <cyrien3519@gmail.com>
2018-06-14 12:03:52 -0600

    1.4b

--- src/main/java/com/cyr1en/mcutils/PluginUpdater.java
+++ src/main/java/com/cyr1en/mcutils/PluginUpdater.java
@@ -1,16 +1,8 @@
 package com.cyr1en.mcutils;
 
 import com.cyr1en.mcutils.utils.FileUtil;
-import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
 import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -167,33 +159,4 @@ public class PluginUpdater {
         return downloadURL;
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onJoin(PlayerJoinEvent event) {
-        if(!newVersionAvailable())
-            return;
-        String v = version.replaceAll("[a-zA-z: ]", "");
-        if(event.getPlayer().isOp()) {
-            try {
-                if(Class.forName("org.spigotmc.SpigotConfig") != null) {
-                    BaseComponent[] textComponent = new ComponentBuilder("[")
-                            .color(ChatColor.GOLD)
-                            .append("CommandPrompter")
-                            .color(ChatColor.GREEN)
-                            .append("]")
-                            .color(ChatColor.GOLD)
-                            .append(" A new update is available: ")
-                            .color(ChatColor.AQUA)
-                            .append(v)
-                            .color(ChatColor.YELLOW)
-                            .event(new ClickEvent(ClickEvent.Action.OPEN_URL, downloadURL))
-                            .event(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to update").create()))
-                            .create();
-                    event.getPlayer().spigot().sendMessage(textComponent);
-                }
-            } catch (ClassNotFoundException e) {
-                String msg = org.bukkit.ChatColor.translateAlternateColorCodes('&', "&6[&aCommandPrmpter&6] &bA new update is available: &e" + v);
-                event.getPlayer().sendRawMessage(msg);
-            }
-        }
-    }
 }
